0fd46fc3800f7f54c060493d74cbd9c7b1b657f8
Avan Singh <avansingh085@gmail.com>
2025-04-27 10:25:05 +0530

    update

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3001"; 
+const BASE_URL = "https://chat-backend-1-3dgt.onrender.com"; 
 
 export const getToken = () => localStorage.getItem("token");
 
